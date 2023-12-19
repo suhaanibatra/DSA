b1f42921aeee8ebf83fcb75114b910b3e80dfdd7
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2023-12-19 13:23:47 +0530

    Create 1_to_n.cpp

---
+++ Basic Recursion/1_to_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printN(int n){
+  if(n <= 0) return;
+  printN(n - 1);
+  cout << n << " ";
+}
+
+int main() {
+  printN(5);
+}
