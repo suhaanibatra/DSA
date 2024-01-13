fdddc9c6048c81d18d198b8eb4da1f2acbd91900
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2024-01-13 20:13:00 +0530

    Create merge_sort.cpp

---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+void merge(std::vector<int>& arr, int low, int mid, int high) {
+
+    std::vector<int> temp;
+
+ 
+
+    int left = low;
+
+    int right = mid + 1;
+
+ 
+
+    while (left <= mid && right <= high) {
+
+        if (arr[left] <= arr[right]) {
+
+            temp.push_back(arr[left]);
+
+            left++;
+
+        } else {
+
+            temp.push_back(arr[right]);
+
+            right++;
+
+        }
+
+    }
+
+ 
+
+    while (left <= mid) {
+
+        temp.push_back(arr[left]);
+
+        left++;
+
+    }
+
+ 
+
+    while (right <= high) {
+
+        temp.push_back(arr[right]);
+
+        right++;
+
+    }
+
+ 
+
+    for (int i = low; i <= high; i++) {
+
+        arr[i] = temp[i - low];
+
+    }
+
+}
+
+ 
+
+void mergeSort(std::vector<int>& arr, int l, int r) {
+
+    if (l >= r) {
+
+        return;
+
+    }
+
+ 
+
+    int mid = (l + r) / 2;
+
+    mergeSort(arr, l, mid);
+
+    mergeSort(arr, mid + 1, r);
+
+    merge(arr, l, mid, r);
+
+}
