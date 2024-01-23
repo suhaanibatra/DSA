755d8b90d971727fb8c30633aa71ab9981bccb78
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2024-01-23 22:51:08 +0530

    Update count_number_of_rotations.cpp

--- Binary Search/count_number_of_rotations.cpp
+++ Binary Search/count_number_of_rotations.cpp
@@ -1,14 +1,30 @@
 int findKRotation(vector<int> &arr){
     int n = arr.size();
     int start = 0, end = n - 1;
+    int ans = 1e9+7;
+    int index = -1;
 
-
-    while(start < end){
+    while(start <= end){
         int mid = start + (end - start)/2;
-        if(arr[start] < arr[mid] && arr[mid] < arr[end]) return start;
-        if(arr[mid] > arr[end]) start = mid + 1;
-        else end = mid;
-    }  
 
-    return start;
+        //left part is sorted
+        if(arr[start] <= arr[mid]){
+            if(arr[start] < ans){
+                ans = arr[start];
+                index = start;
+            }
+            start = mid + 1;
+        }
+        //right part is sorted
+        else{
+            if(arr[mid] < ans){
+                ans = arr[mid];
+                index = mid;
+            }
+
+            end = mid - 1;
+        }
+    } 
+
+    return index;
 }
