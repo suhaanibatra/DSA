73a286404c05200d7e8e20fdd32d4fc4f0070a14
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2024-02-27 23:46:40 +0530

    Create remove_all_occurrences_of_substring.cpp

---
+++ Strings/remove_all_occurrences_of_substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int found = s.find(part);
+
+        while(found != string:: npos){
+            s.erase(found, part.length());
+            found = s.find(part);
+        }
+
+        return s;
+    }
+};
