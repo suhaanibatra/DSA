587f1a9f244a574a856ef25d02534d5c7fc36e9e
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2024-03-11 00:49:30 +0530

    Create March10_intersection_of_two_arrays.cpp

---
+++ Leetcode-POTD/March10_intersection_of_two_arrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int m = nums1.size(), n = nums2.size();
+
+        int i = 0, j = 0;
+        vector<int> ans;
+        while(i < m && j < n){
+            if(nums1[i] == nums2[j]){
+                if(ans.size() == 0 || ans.back() != nums1[i]){
+                    ans.push_back(nums1[i]);
+                }   
+                i++;
+                j++;
+            }
+            else if(nums1[i] > nums2[j]) j++;
+            else i++;
+        }
+        return ans;
+    }
+};
